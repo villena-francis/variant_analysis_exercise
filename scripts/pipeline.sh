656ec80d078c993866a4d0e7b892dc15de6e5abe
Francisco J. Villena <francis.villena@hotmail.es>
2024-02-12 15:41:31 +0100

    Add conversion of SAM to BAM and filling in mate information

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -9,3 +9,6 @@ do
     bwa mem -R "@RG\tID:exercise\tSM:${sample}" genome/*.fa \
         raw_data/${sample}_1.fq.gz raw_data/${sample}_2.fq.gz \
         > alignment/${sample}.sam
+    #Convert the SAM file to BAM and fill in mate information
+    samtools fixmate -O bam alignment/${sample}.sam \
+             alignment/${sample}_fixmate.bam
