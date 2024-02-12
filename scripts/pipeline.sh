0ae3193bec70b525f8e38c125b20e83c6d619e32
Francisco J. Villena <francis.villena@hotmail.es>
2024-02-12 15:25:16 +0100

    Add alignment of raw sequence data to the reference genome

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,2 +1,11 @@
 #Create genome index
 bwa index genome/*.fa
+
+mkdir -p alignment
+
+for sample in normal tumor
+do
+    #Aligment of raw seq data from each sample to the reference genome
+    bwa mem -R "@RG\tID:exercise\tSM:${sample}" genome/*.fa \
+        raw_data/${sample}_1.fq.gz raw_data/${sample}_2.fq.gz \
+        > alignment/${sample}.sam
