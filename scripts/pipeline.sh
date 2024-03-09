47c0be2295fa0493abcd8faf824fab401850b61c
Francisco J. Villena <francis.villena@hotmail.es>
2024-03-09 14:18:00 +0100

    Improve code readability of echo statements

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,77 +1,80 @@
-echo -e "\e[34m##############################################\e[0m"
-echo -e "\e[34m##### Starting variant analysis pipeline #####\n\e[0m"
+blue="\e[34m"
+endblue="\e[0m"
 
-echo -e "\e[34mCreating genome index...\e[0m"
+echo -e "${blue}##############################################${endblue}"
+echo -e "${blue}##### Starting variant analysis pipeline #####\n${endblue}"
+
+echo -e "${blue}Creating genome index...${endblue}"
 
 bwa index genome/*.fa
 
-echo -e "\e[34mDone\n\e[0m"
+echo -e "${blue}Done\n${endblue}"
 
 mkdir -p alignment
 mkdir -p calling
 
 for sample in normal tumor
 do
-    echo -e "\e[34mAligning raw seq data from each ${sample} sample to the reference genome...\e[0m"
+    echo -e "${blue}Aligning raw seq data from each ${sample} sample to the reference genome...${endblue}"
     
     bwa mem -R "@RG\tID:exercise\tSM:${sample}" genome/*.fa \
         raw_data/${sample}_1.fq.gz raw_data/${sample}_2.fq.gz \
         > alignment/${sample}.sam
     
-    echo -e "\e[34mDone\n\e[0m" 
-    echo -e "\e[34mConverting the ${sample} SAM file to BAM and filling in mate information...\e[0m"
+    echo -e "${blue}Done\n${endblue}" 
+    echo -e "${blue}Converting the ${sample} SAM file to BAM and filling in mate information...${endblue}"
     
     samtools fixmate -O bam alignment/${sample}.sam \
              alignment/${sample}_fixmate.bam
     
-    echo -e "\e[34mDone\n\e[0m"
-    echo -e "\e[34mSorting the ${sample} BAM file by coordinates...\e[0m"
+    echo -e "${blue}Done\n${endblue}"
+    echo -e "${blue}Sorting the ${sample} BAM file by coordinates...${endblue}"
     
     samtools sort -O bam -o alignment/${sample}_sorted.bam \
              alignment/${sample}_fixmate.bam
 
-    echo -e "\e[34mDone\n\e[0m"       
-    echo -e "\e[34mRemoving PCR duplicates and generate a refined ${sample} BAM file...\e[0m"
+    echo -e "${blue}Done\n${endblue}"       
+    echo -e "${blue}Removing PCR duplicates and generate a refined ${sample} BAM file...${endblue}"
    
     samtools rmdup -S alignment/${sample}_sorted.bam \
              alignment/${sample}_refined.bam
     
-    echo -e "\e[34mDone\n\e[0m"
-    echo -e "\e[34mIndexing the refined ${sample} BAM file...\e[0m"
+    echo -e "${blue}Done\n${endblue}"
+    echo -e "${blue}Indexing the refined ${sample} BAM file...${endblue}"
     
     samtools index alignment/${sample}_refined.bam
 
-    echo -e "\e[34mDone\n\e[0m"
-    echo -e "\e[34mCalculating the most likely genotype for ${sample} sample...\e[0m"
+    echo -e "${blue}Done\n${endblue}"
+    echo -e "${blue}Calculating the most likely genotype for ${sample} sample...${endblue}"
     
     bcftools mpileup -Ou -f genome/*.fa -T intervals/*.bed\
              alignment/${sample}_refined.bam | bcftools call -vmO z -o \
              calling/${sample}_rawcalls.vcf.gz
 
-    echo -e "\e[34mDone\n\e[0m" 
-    echo -e "\e[34mCSorting and remove duplicates...\e[0m"
+    echo -e "${blue}Done\n${endblue}" 
+    echo -e "${blue}CSorting and remove duplicates...${endblue}"
     
     bcftools sort -Ou calling/${sample}_rawcalls.vcf.gz | bcftools norm -d all -O z -o \
              calling/${sample}_sorted_dedup.vcf.gz
 
-    echo -e "\e[34mDone\n\e[0m" 
-    echo -e "\e[34mCreating the index for the vcf ${sample} file...\e[0m"
+    echo -e "${blue}Done\n${endblue}" 
+    echo -e "${blue}Creating the index for the vcf ${sample} file...${endblue}"
     
     bcftools index calling/${sample}_sorted_dedup.vcf.gz
 
-    echo -e "\e[34mDone\n\e[0m"
-    echo -e "\e[34mRetrieving only calls with high quality in ${sample} sample...\e[0m"
+    echo -e "${blue}Done\n${endblue}"
+    echo -e "${blue}Retrieving only calls with high quality in ${sample} sample...${endblue}"
    
     bcftools filter -i 'QUAL>=20&&DP>=20' calling/${sample}_sorted_dedup.vcf.gz
 
-    echo -e "\e[34mDone\n\e[0m"
+    echo -e "${blue}Done\n${endblue}"
 done
 
-echo -e "\e[34mMaking intersection of detected variants in the normal and tumor samples from the patient...\e[0m"
+echo -e "${blue}Making intersection of detected variants in the normal and tumor samples from the patient...${endblue}"
 
 bcftools isec -i 'QUAL>=20&&DP>=20' calling/normal_sorted_dedup.vcf.gz calling/tumor_sorted_dedup.vcf.gz \
          -p calling/intersection
 
-echo -e "\e[34mDone\n\e[0m"
-echo -e "\e[34m#### variant analysis pipeline completed #####\e[0m"
-echo -e "\e[34m##############################################\e[0m"
+echo -e "${blue}Done\n${endblue}"
+echo -e "${blue}#### variant analysis pipeline completed #####${endblue}"
+echo -e "${blue}##############################################${endblue}"
